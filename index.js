6bac81d09c1ed255aae43611c530ca15f40c3a63
nitinsareen <nitin.sareen.mca@gmail.com>
2021-04-07 11:23:37 +0530

    Updated to fetch data from custom module

--- index.js
+++ index.js
@@ -2,16 +2,23 @@
 let express = require("express");
 let app = express();
 
+//Import custom module for cities data
+let citiesRepo = require("./repos/citiesRepo");
+
 let router = express.Router();
 
 //Send array
-let cities = [
-  { id: 1, name: "Boston" },
-  { id: 2, name: "Chicago" },
-  { id: 3, name: "New York" },
-  { id: 4, name: "Cincinati" },
-  { id: 5, name: "Houston" },
-];
+// let cities = [
+//   { id: 1, name: "Boston" },
+//   { id: 2, name: "Chicago" },
+//   { id: 3, name: "New York" },
+//   { id: 4, name: "Cincinati" },
+//   { id: 5, name: "Houston" },
+// ];
+
+//Initialize cities array with module import
+let cities = citiesRepo.get();
+
 //Default get for cities
 router.get("/", function (req, res, next) {
   //res.send("Boston");
@@ -29,5 +36,5 @@ app.use("/api/v1", router);
 
 //create the server for port 5000
 const server = app.listen(5000, function () {
-  console.log("Server listening on port 5000...");
+  console.log("Server listening on  http://localhost:5000/api/v1 ...");
 });
