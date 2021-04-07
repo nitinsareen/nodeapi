88f4f184ea9c46530a5b5cff1d5359cada718454
nitinsareen <nitin.sareen.mca@gmail.com>
2021-04-07 10:32:02 +0530

    Updated to send array as default

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ let app = express();
 
 let router = express.Router();
 
+//Send array
+let cities = [
+  { id: 1, name: "Boston" },
+  { id: 2, name: "Chicago" },
+  { id: 3, name: "New York" },
+  { id: 4, name: "Cincinati" },
+  { id: 5, name: "Houston" },
+];
 //Default get for cities
 router.get("/", function (req, res, next) {
-  res.send("Boston");
+  //res.send("Boston");
+  res.send(cities);
 });
 
 //Configure the prefix for router
